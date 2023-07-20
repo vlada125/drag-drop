6663af39871a85a615b8eee658fe690f067f5c7c
vlada <87866630+vlada125@users.noreply.github.com>
2023-07-21 00:39:20 +0300

    fix imge

--- src/components/ToolBar/index.tsx
+++ src/components/ToolBar/index.tsx
@@ -19,7 +19,7 @@ export const ToolBar: FC<ToolBarProps> = (props) => {
       <div className="absolute bg-white opacity-90 w-full h-full left-0 top-0 rounded-[10px]" />
       {toolOptions.map((option, index) => (
         <div key={`tool-option${index}`} className={`flex flex-col items-center py-[5px] w-full px-1 relative cursor-pointer [&>.sub-tools]:hover:opacity-100 [&>.sub-tools]:hover:flex select-none`} style={{ zIndex: index }}>
-          {/* <img src={option.icon} alt={'option icon'} /> */}
+          <img src={option.icon} alt={'option icon'} />
           <p className="text-center text-neutral-500 text-xs font-medium mt-[3px]">{option.label}</p>
           {!!option.subTools && (
             <div className="opacity-0 absolute sub-tools left-[100%] top-0 hidden flex-col items-center w-[90px] rounded-[10px] overflow-hidden card-shadow py-1 transition-all duration-500"  style={{ zIndex: index }}>
